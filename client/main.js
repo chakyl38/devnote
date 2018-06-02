9abd8679ab8be37a2ab856b8d89e0fe686db430e
Kyle Chapman <kylechapman09@gmail.com>
2018-06-02 06:56:25 -0500

    added materialize css and finsihed up adding ideas backend

--- client/main.js
+++ client/main.js
@@ -1,5 +1,11 @@
 import { Template } from 'meteor/templating';
 import { Notes } from '../lib/collections';
+import { Accounts } from 'meteor/accounts-base';
+
+// Accounts config
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY'
+});
 import './main.html';
 
 
@@ -12,5 +18,38 @@ Template.body.helpers({
   // ]
   notes(){
     return Notes.find({});
+  }
+});
+
+// ADDING IDEAS TO DB
+Template.add.events({
+  'submit .add-form': function(){
+    event.preventDefault();
+    // GET INPUT VALUE
+    const target = event.target;
+    const text = target.text.value;
+
+    // INSERT IDEA INTO COLLECTION
+   /* Notes.insert({
+      text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    }); */
+
+    Meteor.call('notes.insert', text);
+
+    // CLEAR FORM
+    target.text.value = "";
+    $('#addModal').modal('close');
+    return false;
+  }
+});
+
+Template.note.events({
+  'click .delete-note': function(){
+   // Notes.remove(this._id);
+   Meteor.call('notes.remove', this);
+    return false;
   }
 });
